c67bd9d7225a8a31fd48d3153cfeb32f0e8eda59
tabakazu <tkazz.bd1@gmail.com>
2020-08-03 01:46:00 +0900

    add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'homes#show'
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
